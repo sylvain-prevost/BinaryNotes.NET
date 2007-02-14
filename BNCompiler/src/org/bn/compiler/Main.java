790cbffe1a46a3263e9585a62e2de9d48c0496cc
akira_ag <akira_ag@6e58b0a2-a920-0410-943a-aae568831f16>
2007-02-14 19:14:20 +0000

    Version is prepared for work

--- BNCompiler/src/org/bn/compiler/Main.java
+++ BNCompiler/src/org/bn/compiler/Main.java
@@ -40,7 +40,7 @@ import org.lineargs.LineArgsParser;
 
 
 public class Main {
-    private final static String version = "1.4.2";
+    private final static String version = "1.5.0";
     private LineArgsParser     parser    = new LineArgsParser();
     private CompilerArgs arguments = null;
     
