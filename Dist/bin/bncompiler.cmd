b8d3e5a816395d5c06bad637412ddd4064d6c860
akira_ag <akira_ag@6e58b0a2-a920-0410-943a-aae568831f16>
2007-03-04 18:43:28 +0000

    Migrated to new JDeveloper

--- Dist/bin/bncompiler.cmd
+++ Dist/bin/bncompiler.cmd
@@ -9,4 +9,7 @@ shift
 	shift
 goto setupArgs
 :doneStart
-java -classpath "%BN_HOME%/../depends/lineargs.jar;%BN_HOME%/../depends/antlr.jar;%BN_HOME%/../depends/activation.jar;%BN_HOME%/../depends/jaxb-api.jar;%BN_HOME%/../depends/jaxb-impl.jar;%BN_HOME%/../depends/jaxb1-impl.jar;%BN_HOME%/../depends/jsr173_1.0_api.jar;%BN_HOME%/../lib/java/binarynotes.jar;%BN_HOME%/../bin/bncompiler.jar" org.bn.compiler.Main %CMD_LINE_ARGS%
+rem for Java 5
+rem java -classpath "%BN_HOME%/../depends/lineargs.jar;%BN_HOME%/../depends/antlr.jar;%BN_HOME%/../depends/activation.jar;%BN_HOME%/../depends/java5/jaxb-api.jar;%BN_HOME%/../depends/java5/jaxb-impl.jar;%BN_HOME%/../depends/java5/jaxb1-impl.jar;%BN_HOME%/../depends/java5/jsr173_1.0_api.jar;%BN_HOME%/../lib/java/binarynotes.jar;%BN_HOME%/../bin/bncompiler.jar" org.bn.compiler.Main %CMD_LINE_ARGS%
+
+java -classpath "%BN_HOME%/../depends/lineargs.jar;%BN_HOME%/../depends/antlr.jar;%BN_HOME%/../depends/activation.jar;%BN_HOME%/../lib/java/binarynotes.jar;%BN_HOME%/../bin/bncompiler.jar" org.bn.compiler.Main %CMD_LINE_ARGS%
