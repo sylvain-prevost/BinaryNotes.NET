0499db29e39c0096545eec79ba47264970fc7911
akira_ag <akira_ag@6e58b0a2-a920-0410-943a-aae568831f16>
2007-09-09 13:26:06 +0000

    Mac OS support

--- Dist/bin/bncompiler.sh
+++ Dist/bin/bncompiler.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BN_HOME=`dirname $0`
 echo $BN_HOME
-java -classpath $BN_HOME/../depends/lineargs.jar:$BN_HOME/../depends/antlr.jar:$BN_HOME/../depends/activation.jar:$BN_HOME/../depends/java5/jaxb\-api.jar:$BN_HOME/../depends/java5/jaxb\-impl.jar:$BN_HOME/../depends/java5/jaxb1\-impl.jar:$BN_HOME/../depends/java5/jsr173_1.0_api.jar:$BN_HOME/../lib/java/binarynotes.jar:$BN_HOME/../bin/bncompiler\-java5.jar org.bn.compiler.Main $*
+java -classpath $BN_HOME/../depends/lineargs.jar:$BN_HOME/../depends/antlr.jar:$BN_HOME/../depends/activation.jar:$BN_HOME/../depends/java5/jaxb\-api.jar:$BN_HOME/../depends/java5/jaxb\-impl.jar:$BN_HOME/../depends/java5/jaxb1\-impl.jar:$BN_HOME/../depends/java5/jsr173_1.0_api.jar:$BN_HOME/../lib/java/binarynotes.jar:$BN_HOME/../bin/bncompiler.jar org.bn.compiler.Main $*
 
 
