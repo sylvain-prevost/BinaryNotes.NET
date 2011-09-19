4841063ff49931d7f2f2cfc0250d8e76046838db
akira_ag <akira_ag@6e58b0a2-a920-0410-943a-aae568831f16>
2011-09-19 15:18:17 +0000

    GPL header fixes-try#2

--- BNCompiler/src/test/org/bn/compiler/parser/ASNParserTest.java
+++ BNCompiler/src/test/org/bn/compiler/parser/ASNParserTest.java
@@ -1,21 +1,20 @@
 /*
- * Copyright 2006 Abdulla G. Abdurakhmanov (abdulla.abdurakhmanov@gmail.com).
- * 
- * Licensed under the GPL, Version 2 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.gnu.org/copyleft/gpl.html
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * With any your questions welcome to my e-mail 
- * or blog at http://abdulla-a.blogspot.com.
+ Copyright 2006-2011 Abdulla Abdurakhmanov (abdulla@latestbit.com)
+ Original sources are available at www.latestbit.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
+
 package test.org.bn.compiler.parser;
 
 import junit.framework.TestCase;
