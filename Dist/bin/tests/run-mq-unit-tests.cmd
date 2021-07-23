69e74e383ebb8805cc55e43704660bbba2d44658
Sylvain Prevost <sylvain.prevost@gmail.com>
2021-07-23 01:19:03 -0500

    Cleanup: remove unused/moot files.

--- Dist/bin/tests/run-mq-unit-tests.cmd
+++
@@ -1,1 +0,0 @@
-java -cp ../../lib/java/unit-tests/binarynotes-mq.jar;../../lib/java/unit-tests/binarynotes-mq-ut.jar;../../lib/java/binarynotes.jar;../../depends/junit.jar;../../lib/java/binarynotes-mq.jar test.org.bn.mq.AllTests
